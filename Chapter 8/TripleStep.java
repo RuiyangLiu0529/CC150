15a63cab59d285d9490ec456c28212e101bdd4de
Ruiyang Liu <ruiyang@ruiyangs-MacBook-Pro.local>
2016-12-23 14:17:42 -0500

    DEC 23 2016

--- Chapter 8/TripleStep.java
+++ Chapter 8/TripleStep.java
@@ -6,7 +6,9 @@ class TripleStep {
 	}
 
 	int countWaysHelper(int i, int[] memo) {
-		if(i<=3) return 1;
+		if(i == 1) return 1;
+		if(i == 2) return 2;
+		if(i == 3) return 3;
 		if(memo[i] == 0) {
 			memo[i] = countWaysHelper(i-1, memo) + countWaysHelper(i-2, memo) + countWaysHelper(i-3, memo);
 		}
@@ -15,6 +17,6 @@ class TripleStep {
 
 	public static void main(String[] args) {
 		TripleStep result = new TripleStep();
-		System.out.println(result.countWays(5) + "the right answer is 5");
+		System.out.println(result.countWays(5) + "the right answer is ");
 	}
-}
+}	
